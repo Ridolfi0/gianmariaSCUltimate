a4be61974db77d9b04cedee66cee705418656194
CastorriJ <jessica.castorri.stud@ispascalcomandini.it>
2025-06-10 14:50:54 +0200

    aggiunte in login.js per la creazione cartelle

--- commit.bat
+++ commit.bat
@@ -1,5 +1,5 @@
 @echo off
 call git add --a 
-call git commit --m "impegni funzionante ma da migliorare" 
+call git commit --m "aggiunte in login.js per la creazione cartelle" 
 call git pull 
 call git push 
